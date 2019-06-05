3684644fc59adc992ba4e73644b9bcdcbaafcb3e
nitianyang <1214839514@qq.com>
2019-06-05 19:40:09 +0800

    add user valid model

--- src/main/java/com/agree/bank/zjjg/ZjjgApplication.java
+++ src/main/java/com/agree/bank/zjjg/ZjjgApplication.java
@@ -1,8 +1,10 @@
 package com.agree.bank.zjjg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.agree.bank.zjjg.dao.UserDao")
 @SpringBootApplication
 public class ZjjgApplication {
 
--- src/main/java/com/agree/bank/zjjg/controller/UserController.java
+++ src/main/java/com/agree/bank/zjjg/controller/UserController.java
@@ -1,14 +1,64 @@
 package com.agree.bank.zjjg.controller;
 
+import com.agree.bank.zjjg.pojo.Users;
+import com.agree.bank.zjjg.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
 public class UserController {
 
-    @RequestMapping("/hello")
-    public String helloword(String name){
+    @Autowired
+    private UserService userService;
 
-        return name+"说了句你好springboot";
+    @RequestMapping("/hello")
+    public String helloword(Integer id){
+        System.out.println(id+"===============================");
+        System.out.println(userService.selectById(id));
+        return "test";
+    }
+
+    /**
+     * 跳转进入登录界面
+     * @return
+     */
+    @RequestMapping("/index")
+    public  String index(){
+        return "login";
+    }
+
+    /**
+     * 提供表单校验功能
+     * @param users
+     * @param bindingResult
+     * @param request
+     * @return
+     */
+    @RequestMapping("/valid")
+    public String valid(@Valid  Users users , BindingResult bindingResult , HttpServletRequest request){
+
+        if(!bindingResult.hasErrors()){
+           System.out.println("校验成功");
+//           进入系统界面
+            return "userView";
+        }
+        List <FieldError>list =bindingResult.getFieldErrors();
+        Map <String ,Object >map = new HashMap<>();
+
+        for(int i=0;i<list.size();i++ ){
+            map.put(list.get(i).getField(),list.get(i).getDefaultMessage());
+        }
+        request.setAttribute("err",map);
+//        校验失败返回请求界面并提示错误.....
+        return "login";
     }
 }
